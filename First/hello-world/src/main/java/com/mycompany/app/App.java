2e0d8c9e37605cced77af064b64aa7e0017508ae
Akar <akarsihar@gmail.com>
2023-11-08 19:23:52 +0530

    creating jar file

--- First/hello-world/src/main/java/com/mycompany/app/App.java
+++ First/hello-world/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int num1 = 100;
+        int num2 = 10;
+
+        System.out.println("Addition of " + num1 + " and " + num2 + " is " + calculator.add(num1, num2));
+        System.out.println("Subtraction of " + num1 + " and " + num2 + " is " + calculator.sub(num1, num2));
+        System.out.println("Multiplication of " + num1 + " and " + num2 + " is " + calculator.mul(num1, num2));
+        System.out.println("Division of " + num1 + " and " + num2 + " is " + calculator.div(num1, num2));
+
     }
 }
